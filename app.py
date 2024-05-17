d93c28cb9b9a3adc73f53705b003dbb3d5c5a231
Pavel <duminpn@gmail.com>
2024-05-17 16:57:07 +0300

    small comments fixes

--- app.py
+++ app.py
@@ -68,16 +68,11 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# Accept user input
 if prompt := st.chat_input("What is up?"):
-    # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
-    # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
     response = qa_chain({"query": prompt})
-    # Display assistant response in chat message container
     with st.chat_message("assistant"):
         st.write(response['result'])
-    # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response['result']})
